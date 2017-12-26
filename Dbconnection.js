34de8200a161f48decdcaa38656e0cdce999d0df
Ton Nguyen <hoangton1996@gmail.com>
2017-12-26 22:29:30 +0700

    Update db connect

--- Dbconnection.js
+++ Dbconnection.js
@@ -7,18 +7,18 @@ var mysql = require('mysql');
 //  database:'websitebansach'
 
      //#Host Heroku
-     // host:'us-cdbr-iron-east-05.cleardb.net',
-     // user: 'ba07e414f4d8e4',
-     // password:'b51bed0c',
-     // database:'heroku_1873b262d36b3f6',
-     // port: '3306'
-
-     //#Host freesqldatabase
-     host:'sql12.freesqldatabase.com',
-     user: 'sql12211607',
-     password:'GHsj9tM2jA',
-     database:'sql12211607',
+     host:'us-cdbr-iron-east-05.cleardb.net',
+     user: 'ba07e414f4d8e4',
+     password:'b51bed0c',
+     database:'heroku_1873b262d36b3f6',
      port: '3306'
+
+     // //#Host freesqldatabase
+     // host:'sql12.freesqldatabase.com',
+     // user: 'sql12211607',
+     // password:'GHsj9tM2jA',
+     // database:'sql12211607',
+     // port: '3306'
 
 });
 
