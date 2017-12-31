4d27b17630d8d695e8170f33b492eb37fcdfb0b5
Ton Nguyen <hoangton1996@gmail.com>
2017-12-31 11:59:27 +0700

    Update tính năng đăng ký

--- Routes/routeKhachHang.js
+++ Routes/routeKhachHang.js
@@ -14,133 +14,172 @@ var jwt = require('jsonwebtoken');
 //     Login(decoded.Email, decoded.MatKhau, res);
 // });
 
-router.post('/dangky',function(req,res,next)
+router.post('/dangky', function (req, res, next)
 {
     var md5 = crypto.createHash('md5').update(req.body.MatKhau).digest("hex");
     var query = "INSERT INTO khachhang (HoTenKhachHang,Email,MatKhau,TienNo,LoaiKhachHang,SoXuTichLuy) Values (?,?,?,0,0,0)";
-    
-    var token = jwt.sign(req.body, 'tohiti');
-    db.query(query,[req.body.HoTenKhachHang,req.body.Email,md5], function (error, results, fields) {
-        if (error) {
-        //   console.log("error ocurred",error);
-          res.send({
-            "code":400,
-            "failed":"error ocurred"
-          })
-        }else{
-        //   console.log('The solution is: ', results);
-          res.send({
-            "code":200,
-            "success":"user registered sucessfully",
-            "token": token,
-              });
-        }
-        });
-});
-function Login(email,mk,res)
-{
-    var md5 = crypto.createHash('md5').update(mk).digest("hex");
-    db.query('SELECT * FROM khachhang WHERE Email = ?',[email], function (error, results, fields) {
-    if (error) {
-      // console.log("error ocurred",error);
-      res.send({
-        "code":400,
-        "failed":"error ocurred"
-      })
-    }else{
-      // console.log('The solution is: ', results);
-      if(results.length >0){
-        if(results[0].MatKhau == md5)
+    db.query("select * from khachhang where Email=?", [req.body.Email], function (error, result, fields)
+    {
+        if (error)
         {
-            var user = {
-                MaKhachHang:results[0].MaKhachHang,
-                HoTenKhachHang: results[0].HoTenKhachHang,
-                Email: results[0].Email,
-                MatKhau: mk,
-            }
-            var token = jwt.sign(user, 'tohiti');
+            console.log('có lỗi');
 
-          res.send({
-            "code":200,
-            "message":"Login sucessfull",
-              "token": token,
-              });
+            res.send({
+                "message": error,
+                "code": "400",
+            })
         }
         else
+        {
+            console.log('không lỗi');
+            if (result.length != 0)
             {
-          res.send({
-            "code":204,
-            "message":"Email and password does not match"
-              });
+                res.send({
+                    "message": "Email này đã được sử dụng",
+                    "code": "400",
+                })
+
+            }
+            else
+            {
+                var token = jwt.sign(req.body, 'tohiti');
+                db.query(query,[req.body.HoTenKhachHang,req.body.Email,md5], function (error, results, fields) {
+                    if (error) {
+                        //   console.log("error ocurred",error);
+                        res.send({
+                            "code":400,
+                            "message":"error ocurred"
+                        })
+                    }else{
+                        //   console.log('The solution is: ', results);
+                        res.send({
+                            "code":200,
+                            "message":"Đăng ký thành công",
+                            "token": token,
+                        });
+                    }
+                });
+            }
         }
-      }
-      else{
-        res.send({
-          "code":204,
-          "message":"Email does not exists"
-            });
-      }
-    }
+    })
+});
+
+function Login(email, mk, res)
+{
+    var md5 = crypto.createHash('md5').update(mk).digest("hex");
+    db.query('SELECT * FROM khachhang WHERE Email = ?', [email], function (error, results, fields)
+    {
+        if (error)
+        {
+            // console.log("error ocurred",error);
+            res.send({
+                "code": 400,
+                "failed": "error ocurred"
+            })
+        } else
+        {
+            // console.log('The solution is: ', results);
+            if (results.length > 0)
+            {
+                if (results[0].MatKhau == md5)
+                {
+                    var user = {
+                        MaKhachHang: results[0].MaKhachHang,
+                        HoTenKhachHang: results[0].HoTenKhachHang,
+                        Email: results[0].Email,
+                        MatKhau: mk,
+                    }
+                    var token = jwt.sign(user, 'tohiti');
+
+                    res.send({
+                        "code": 200,
+                        "message": "Đăng nhập thành công",
+                        "token": token,
+                    });
+                }
+                else
+                {
+                    res.send({
+                        "code": 204,
+                        "message": "Email hoặc mật khẩu không đúng"
+                    });
+                }
+            }
+            else
+            {
+                res.send({
+                    "code": 204,
+                    "message": "Email không tồn tại"
+                });
+            }
+        }
     });
 }
-router.post('/dangnhap?',function(req,res,next)
+
+router.post('/dangnhap?', function (req, res, next)
 {
     Login(req.body.Email, req.body.MatKhau, res);
 });
 
-router.get('/thongtin?',function(req,res,next)
-{
-    console.log('/token');
-     var token = req.headers.authorization;
-     var decoded = jwt.verify(token, 'tohiti');
-     console.log(decoded);
-     res.send(decoded);
-
-});
-
-router.get('/yeuthich?',function(req,res,next)
+router.get('/thongtin?', function (req, res, next)
 {
     console.log('/token');
     var token = req.headers.authorization;
     var decoded = jwt.verify(token, 'tohiti');
     console.log(decoded);
-    KhachHang.getDanhSachYeuThich(decoded.MaKhachHang,function(err, rows)
+    res.send(decoded);
+
+});
+
+router.get('/yeuthich?', function (req, res, next)
+{
+    console.log('/token');
+    var token = req.headers.authorization;
+    var decoded = jwt.verify(token, 'tohiti');
+    console.log(decoded);
+    KhachHang.getDanhSachYeuThich(decoded.MaKhachHang, function (err, rows)
     {
-        if(err)
+        if (err)
         {
             res.json(err);
-        } else {
+        } else
+        {
             res.json(rows);
         }
     })
 
 });
+
 function getDanhSachSanPham(MaHoaDon)
 {
     var sql = "select * from chitiethoadon INNER join sach  on chitiethoadon.MaSach=sach.MaSach where MaHoaDon=?";
-    return new Promise(function(resolve,reject){
-        db.query(sql, [MaHoaDon], function(err, result){
-            if(err){
+    return new Promise(function (resolve, reject)
+    {
+        db.query(sql, [MaHoaDon], function (err, result)
+        {
+            if (err)
+            {
                 reject(err)
-            }else{
+            } else
+            {
                 resolve(result)
             }
         })
     })
 
 
-
 }
-router.get('/lichsumuahang?', function (req,res,next)
+
+router.get('/lichsumuahang?', function (req, res, next)
 {
     var token = req.headers.authorization;
     var decoded = jwt.verify(token, 'tohiti');
     console.log(decoded);
     var dsDonHang = [];
     var donhang;
-    db.query("select * from hoadon INNER join phieuthutien  on hoadon.MaHoaDon=phieuthutien.MaHoaDon where hoadon.MaKhachHang = ?",[decoded.MaKhachHang], async function (errorHoaDon,resultHoaDon)
+    db.query("select * from hoadon INNER join phieuthutien  on hoadon.MaHoaDon=phieuthutien.MaHoaDon where hoadon.MaKhachHang = ?", [decoded.MaKhachHang], async function (errorHoaDon, resultHoaDon)
     {
-        if(errorHoaDon)
+        if (errorHoaDon)
         {
             console.log('Error' + errorHoaDon)
             res.json(errorHoaDon);
@@ -149,22 +188,22 @@ router.get('/lichsumuahang?', function (req,res,next)
         {
 
 
-            for(i=0; i < resultHoaDon.length; i++)
+            for (i = 0; i < resultHoaDon.length; i++)
             {
-                 var dsSanPham = [];
-                 dsSanPham = await getDanhSachSanPham(resultHoaDon[i].MaHoaDon);
+                var dsSanPham = [];
+                dsSanPham = await getDanhSachSanPham(resultHoaDon[i].MaHoaDon);
                 //console.log('Trang thai hien tai la:', trangthai)
                 donhang = {
                     MaHoaDon: resultHoaDon[i].MaHoaDon,
                     NgayLapHoaDon: resultHoaDon[i].NgayLapHoaDon,
-                    NgayThuTien:resultHoaDon[i].NgayThuTien,
+                    NgayThuTien: resultHoaDon[i].NgayThuTien,
                     TongTienHoaDon: resultHoaDon[i].TongTienHoaDon,
                     DiaChiGiaoHang: resultHoaDon[i].DiaChiGiaoHang,
                     TenNguoiNhan: resultHoaDon[i].TenNguoiNhan,
                     SoDienThoai: resultHoaDon[i].SoDienThoai,
                     SoXuSuDung: resultHoaDon[i].SoXuSuDung,
                     PhiGiaoHang: resultHoaDon[i].PhiGiaoHang,
-                    TrangThai:  resultHoaDon[i].TrangThai,
+                    TrangThai: resultHoaDon[i].TrangThai,
                     dsSanPham: dsSanPham,
                 }
                 dsDonHang.push(donhang);
@@ -177,36 +216,45 @@ router.get('/lichsumuahang?', function (req,res,next)
 });
 
 
-
-
-router.get('/hd/:MaKhachHang?',function(req,res,next){
-    KhachHang.getHoaDonByMaKhachHang(req.params.MaKhachHang,function(err,rows){
-        if(err)
+router.get('/hd/:MaKhachHang?', function (req, res, next)
+{
+    KhachHang.getHoaDonByMaKhachHang(req.params.MaKhachHang, function (err, rows)
+    {
+        if (err)
         {
             res.json(err);
-        } else {
+        } else
+        {
             res.json(rows);
         }
     });
 });
 
 
-router.get('/:id?',function(req,res,next)
+router.get('/:id?', function (req, res, next)
 {
-    if(req.params.id){
-        KhachHang.getKhachHangById(req.params.id,function(err,rows){
-            if(err){
+    if (req.params.id)
+    {
+        KhachHang.getKhachHangById(req.params.id, function (err, rows)
+        {
+            if (err)
+            {
                 res.json(err);
             }
-            else{
+            else
+            {
                 res.json(rows);
             }
         });
-    }else{
-        KhachHang.getAllKhachHang(function(err,rows){
-            if(err){
+    } else
+    {
+        KhachHang.getAllKhachHang(function (err, rows)
+        {
+            if (err)
+            {
                 res.json(err);
-            } else {
+            } else
+            {
                 res.json(rows);
             }
 
