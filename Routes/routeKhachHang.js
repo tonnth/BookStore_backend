c56a5a920c4adac8d934eaba73fd83ad391007e0
Ton Nguyen <hoangton1996@gmail.com>
2018-01-03 15:32:06 +0700

    Thêm tính năng like

--- Routes/routeKhachHang.js
+++ Routes/routeKhachHang.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var KhachHang = require('../Models/KhachHang');
-var Sach=require(('../Models/KhachHang'))
+var Sach = require(('../Models/KhachHang'))
 var crypto = require('crypto');
 var db = require('../Dbconnection');
 var jwt = require('jsonwebtoken');
@@ -44,18 +44,21 @@ router.post('/dangky', function (req, res, next)
             else
             {
 
-                db.query(query,[req.body.HoTenKhachHang,req.body.Email,md5], function (error, results, fields) {
-                    if (error) {
+                db.query(query, [req.body.HoTenKhachHang, req.body.Email, md5], function (error, results, fields)
+                {
+                    if (error)
+                    {
                         //   console.log("error ocurred",error);
                         res.send({
-                            "code":400,
-                            "message":"Lỗi đăng ký. Vui lòng thử lại."
+                            "code": 400,
+                            "message": "Lỗi đăng ký. Vui lòng thử lại."
                         })
-                    }else{
+                    } else
+                    {
                         //   console.log('The solution is: ', results);
                         res.send({
-                            "code":200,
-                            "message":"Đăng ký thành công",
+                            "code": 200,
+                            "message": "Đăng ký thành công",
 
                         });
                     }
@@ -128,9 +131,9 @@ router.get('/thongtin?', function (req, res, next)
     console.log('/token');
     var token = req.headers.authorization;
     var decoded = jwt.verify(token, 'tohiti');
-    db.query("select MaKhachHang,HoTenKhachHang,Email,SoXuTichLuy from khachhang where MaKhachHang=?",[decoded.MaKhachHang],function (err,result)
+    db.query("select MaKhachHang,HoTenKhachHang,Email,SoXuTichLuy from khachhang where MaKhachHang=?", [decoded.MaKhachHang], function (err, result)
     {
-        if(err) res.json(err);
+        if (err) res.json(err);
         else
         {
             res.json(result[0]);
@@ -140,9 +143,9 @@ router.get('/thongtin?', function (req, res, next)
 
 });
 
-router.get('/yeuthich?', function (req, res, next)
+router.get('/dsyeuthich?', function (req, res, next)
 {
-    console.log('/token');
+
     var token = req.headers.authorization;
     var decoded = jwt.verify(token, 'tohiti');
     console.log(decoded);
@@ -158,6 +161,48 @@ router.get('/yeuthich?', function (req, res, next)
     })
 
 });
+
+router.put('/thich', function (req, res, next)
+{
+    var token = req.headers.authorization;
+    var decoded = jwt.verify(token, 'tohiti');
+    console.log('Body:',req.body);
+
+
+        db.query('select * from brkhachhang where MaKhachHang=? and MaSach=?', [decoded.MaKhachHang, req.body.MaSach], function (error, result)
+        {
+            if (error) console.log('Lỗi get brkhachhang:',error);
+            else
+            {
+                if (result.length === 0) //Chư có trong BR khách hàng thì thêm vào
+                {
+                    db.query('insert into brkhachhang (MaKhachHang,MaSach,YeuThich,SoLuongMua,SoLuotXem) values (?,?,1,0,0)', [decoded.MaKhachHang, req.body.MaSach], function (error2, result2)
+                    {
+                        if (error2) console.log('Lỗi thêm brkhachhang:',error2);
+                        else
+                        {
+                            res.send({"code": "success","messsage":"like thêm mới thành công"})
+                        }
+                    })
+                }
+                else //
+                {
+                    db.query('update brkhachhang set YeuThich=? where MaKhachHang=? and MaSach=?', [req.body.like,decoded.MaKhachHang, req.body.MaSach], function (error, result)
+                    {
+                        if (error) console.log('Lỗi update brkhachhang:',error);
+                        else
+                        {
+                            res.send({"code": "success","messsage":"like update thành công"})
+                        }
+                    })
+                }
+            }
+
+        })
+
+
+
+})
 
 function getDanhSachSanPham(MaHoaDon)
 {
@@ -272,28 +317,27 @@ router.get('/:id?', function (req, res, next)
 
 router.post('/dathang?', async function (req, res, next)
 {
-    var noidung ='';
+    var noidung = '';
     var token = req.headers.authorization;
-    var GioHang =req.body.GioHang;
+    var GioHang = req.body.GioHang;
     var decoded = jwt.verify(token, 'tohiti');
     var TongTien = req.body.TongTienHoaDon;
 
 
-
     var SoXuTichLuy = await getSoXuTichLuy(decoded.MaKhachHang);
-    console.log('So xu tich luy ban dau:',SoXuTichLuy)
+    console.log('So xu tich luy ban dau:', SoXuTichLuy)
     console.log('So xu su dung:', req.body.SoXuSuDung)
     SoXuTichLuy -= req.body.SoXuSuDung;
-    console.log('So xu tich luy sau: ',SoXuTichLuy);
+    console.log('So xu tich luy sau: ', SoXuTichLuy);
 
 
     var sql = "update khachhang set SoXuTichLuy=? where MaKhachHang=?";
-    await db.query(sql,[SoXuTichLuy,decoded.MaKhachHang],function (err0, result0)
+    await db.query(sql, [SoXuTichLuy, decoded.MaKhachHang], function (err0, result0)
     {
         // if(err2) res.json(err2);
-        if(err0)
+        if (err0)
         {
-            console.log('Lỗi update số xu: ',err0);
+            console.log('Lỗi update số xu: ', err0);
         }
 
     })
@@ -302,46 +346,46 @@ router.post('/dathang?', async function (req, res, next)
     var query = "INSERT INTO hoadon (NgayLapHoaDon,MaKhachHang,MaKhuVucGiaoHang,DiaChiGiaoHang,TenNguoiNhan,SoDienThoai,SoXuSuDung,TongTienHoaDon,PhiGiaoHang) " +
         "Values (?,?,?,?,?,?,?,?,?)";
     var currentdate = new Date();
-    db.query(query,[currentdate,decoded.MaKhachHang,req.body.MaKhuVucGiaoHang,req.body.DiaChiGiaoHang,req.body.TenNguoiNhan,req.body.SoDienThoai,req.body.SoXuSuDung,TongTien,0],async function (error,result)
+    db.query(query, [currentdate, decoded.MaKhachHang, req.body.MaKhuVucGiaoHang, req.body.DiaChiGiaoHang, req.body.TenNguoiNhan, req.body.SoDienThoai, req.body.SoXuSuDung, TongTien, 0], async function (error, result)
     {
-        if(error)
+        if (error)
         {
             console.log(error);
             res.json(error);
         }
         else
         {
-            for(i=0;i<GioHang.length;i++)
+            for (i = 0; i < GioHang.length; i++)
             {
                 //Update noidung
                 var TenSach = await getTenSach(GioHang[i].MaSach);
-                TenSach=TenSach[0].TenSach;
-                noidung += GioHang[i].SoLuongBan + ' cuốn '+ TenSach+', ';
+                TenSach = TenSach[0].TenSach;
+                noidung += GioHang[i].SoLuongBan + ' cuốn ' + TenSach + ', ';
 
                 //Update số lượng tồn của đầu sách
                 // console.log('CẬP NHẬT SỐ LƯỢNG TỒN')
                 var soLuongTon = await getSoLuongTon(GioHang[i].MaSach);
-                soLuongTon=soLuongTon[0].SoLuongTon;
-                soLuongTon =soLuongTon - GioHang[i].SoLuongBan;
+                soLuongTon = soLuongTon[0].SoLuongTon;
+                soLuongTon = soLuongTon - GioHang[i].SoLuongBan;
 
 
                 var sql = "update sach set SoLuongTon=? where MaSach=?";
-                await db.query(sql, [soLuongTon,GioHang[i].MaSach], function (err2, result2)
+                await db.query(sql, [soLuongTon, GioHang[i].MaSach], function (err2, result2)
                 {
                     // if(err2) res.json(err2);
-                    if(err2)
+                    if (err2)
                     {
-                        console.log('Lỗi update số lượng tồn: ',err2);
+                        console.log('Lỗi update số lượng tồn: ', err2);
                     }
 
                 })
 
                 //Tạo các chi tiết hóa đơn
                 // console.log('TẠO CHI TIẾT HÓA ĐƠN')
-                await db.query('insert into chitiethoadon (MaHoaDon,MaSach,SoLuongBan,GiaBanCu) values(?,?,?,?)',[result.insertId,GioHang[i].MaSach,GioHang[i].SoLuongBan,GioHang[i].GiaBan], function (err3, result3)
+                await db.query('insert into chitiethoadon (MaHoaDon,MaSach,SoLuongBan,GiaBanCu) values(?,?,?,?)', [result.insertId, GioHang[i].MaSach, GioHang[i].SoLuongBan, GioHang[i].GiaBan], function (err3, result3)
                 {
                     // if(err3) res.json(err3);
-                    if(err3)
+                    if (err3)
                     {
                         console.log('Lỗi tạo chi tiết hóa đơn: ', err3);
                     }
@@ -350,16 +394,16 @@ router.post('/dathang?', async function (req, res, next)
 
                 //Cập nhật BR
                 // console.log('CẬP NHẬT BR KHÁCH HÀNG')
-                var brkhachhang = await getBRKhachHang(decoded.MaKhachHang,GioHang[i].MaSach);
-                if(brkhachhang.length===0)
+                var brkhachhang = await getBRKhachHang(decoded.MaKhachHang, GioHang[i].MaSach);
+                if (brkhachhang.length === 0)
                 {
                     console.log('Tạo mới brkhachhang');
-                    await db.query('insert into brkhachhang (MaKhachHang,MaSach,SoLuongMua) values (?,?,?)', [decoded.MaKhachHang,GioHang[i].MaSach, GioHang[i].SoLuongBan], function (err4,result4)
+                    await db.query('insert into brkhachhang (MaKhachHang,MaSach,SoLuongMua) values (?,?,?)', [decoded.MaKhachHang, GioHang[i].MaSach, GioHang[i].SoLuongBan], function (err4, result4)
                     {
                         // if(err4) res.json(err4);
-                        if(err4)
+                        if (err4)
                         {
-                            console.log('Lỗi tạo mới brkhachhang: ',err4);
+                            console.log('Lỗi tạo mới brkhachhang: ', err4);
                         }
 
                     })
@@ -367,9 +411,9 @@ router.post('/dathang?', async function (req, res, next)
                 else
                 {
                     console.log('Update brkhachhang');
-                    var soLuongMua=brkhachhang[0].SoLuongMua;
-                    soLuongMua=soLuongMua + GioHang[i].SoLuongBan;
-                    var query ='update brkhachhang set SoLuongMua='+ soLuongMua +' where MaKhachHang='+ decoded.MaKhachHang+ ' and MaSach='+GioHang[i].MaSach;
+                    var soLuongMua = brkhachhang[0].SoLuongMua;
+                    soLuongMua = soLuongMua + GioHang[i].SoLuongBan;
+                    var query = 'update brkhachhang set SoLuongMua=' + soLuongMua + ' where MaKhachHang=' + decoded.MaKhachHang + ' and MaSach=' + GioHang[i].MaSach;
                     console.log(query);
                     // await db.query('update brkhachhang set SoLuongMua=? where MaKhachHang=? and MaSach=?',[soLuongMua,decoded.MaKHachHang,GioHang[i].MaSach], function (err5,result5)
                     // {
@@ -381,12 +425,12 @@ router.post('/dathang?', async function (req, res, next)
                     //     console.log(result5);
                     // })
 
-                    await db.query(query, function (err5,result5)
+                    await db.query(query, function (err5, result5)
                     {
                         // if(err5) res.json(err5);
-                        if(err5)
+                        if (err5)
                         {
-                            console.log('Lỗi cập nhật brkhachhang: ',err5);
+                            console.log('Lỗi cập nhật brkhachhang: ', err5);
                         }
 
                     })
@@ -395,20 +439,19 @@ router.post('/dathang?', async function (req, res, next)
             }
 
             // console.log('Tạo phiếu thu tiền');
-            await db.query('insert into phieuthutien (MaKhachHang,SoTienThu,NoiDung,TrangThai,MaHoaDon) values(?,?,?,?,?)',[decoded.MaKhachHang,TongTien,noidung,'Chưa thanh toán(Không cho nợ)',result.insertId], function (err6,result6)
+            await db.query('insert into phieuthutien (MaKhachHang,SoTienThu,NoiDung,TrangThai,MaHoaDon) values(?,?,?,?,?)', [decoded.MaKhachHang, TongTien, noidung, 'Chưa thanh toán(Không cho nợ)', result.insertId], function (err6, result6)
             {
                 // if(err6) res.json(err6);
-                if(err6)
+                if (err6)
                 {
-                    console.log('Lỗi tạo phiếu thu tiền: ',err6);
+                    console.log('Lỗi tạo phiếu thu tiền: ', err6);
                 }
 
             })
 
 
-
         }
-        res.send({'code':' đặt hàng thành công'});
+        res.send({'code': ' đặt hàng thành công'});
 
     });
     // res.send({'code':' đặt hàng thành công'});
@@ -431,6 +474,7 @@ function getSoXuTichLuy(MaKhachHang)
         })
     })
 }
+
 function getSoLuongTon(MaSach)
 {
     var sql = "select SoLuongTon from sach where MaSach=?";
@@ -448,12 +492,13 @@ function getSoLuongTon(MaSach)
         })
     })
 }
-function getBRKhachHang(MaKhachHang,MaSach)
+
+function getBRKhachHang(MaKhachHang, MaSach)
 {
     var sql = "select * from brkhachhang where MaKhachHang=? and MaSach=?";
     return new Promise(function (resolve, reject)
     {
-        db.query(sql, [MaKhachHang,MaSach], function (err, result)
+        db.query(sql, [MaKhachHang, MaSach], function (err, result)
         {
             if (err)
             {
@@ -483,4 +528,5 @@ function getTenSach(MaSach)
         })
     })
 }
+
 module.exports = router;
