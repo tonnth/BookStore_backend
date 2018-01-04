34119eb8fee9b8fa9cac5070ceca8a9bb447ce46
Ton Nguyen <hoangton1996@gmail.com>
2018-01-04 13:38:45 +0700

    Update Dbconnection.js

--- Dbconnection.js
+++ Dbconnection.js
@@ -7,11 +7,19 @@ var connection = mysql.createPool({
     // database: 'websitebansach',
 
     // //#Host Heroku
+    //host: 'us-cdbr-iron-east-05.cleardb.net',
+    //user: 'ba07e414f4d8e4',
+    //password: 'b51bed0c',
+    //database: 'heroku_1873b262d36b3f6',
+    //port: '3306'
+    
     host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'ba07e414f4d8e4',
-    password: 'b51bed0c',
-    database: 'heroku_1873b262d36b3f6',
+    user: 'b6876d546ca64a',
+    password: 'ef5bb417',
+    database: '/heroku_e124678025086b6',
     port: '3306'
+    
+    
 
     // //#Host freesqldatabase
     // host:'sql12.freesqldatabase.com',
